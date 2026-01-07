3ef5b288cf5a3ce78f246689563a2884fd0b4ad4
IvanVazquezL <ivanvl_11@hotmail.com>
2026-01-07 01:35:30 +0000

    Implementar QueriesSum

--- PrefixSum/QueriesSum.js
+++ PrefixSum/QueriesSum.js
@@ -1,5 +1,11 @@
 function QueriesSum(nums, i, j) {
+    const prefixSum = [nums[0]];
 
+    for (let i = 1; i < nums.length; i++) {
+        prefixSum[i] = prefixSum[i - 1] + nums[i];
+    }
+
+    return prefixSum[j] - prefixSum[i] + nums[i];
 }
 
 console.log(QueriesSum([1, 2, 3, 4, 5, 6], 1, 3));
