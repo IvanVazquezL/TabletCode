e084b61ba944a51e885c1b6013a8412ff9952613
IvanVazquezL <ivanvl_11@hotmail.com>
2026-03-03 02:42:08 +0000

    merge sorted arrays python corregido

--- merge_sorted_arrays.py
+++ merge_sorted_arrays.py
@@ -1,22 +1,18 @@
-def merge_sorted_arrays(a_1: list[int], a_2: list[int]) -> list[int]:
-    i_1, i_2, new_a = 0, 0, []
+def merge_sorted_arrays(a1: list[int], a2: list[int]) -> list[int]:
+    i1, i2, result = 0, 0, []
+    n1, n2 = len(a1), len(a2)
 
-    while i_1 < len(a_1) and i_2 < len(a_2):
-        if a_1[i_1] < a_2[i_2]:
-            new_a.append(a_1[i_1])
-            i_1 += 1
+    while i1 < n1 and i2 < n2:
+        if a1[i1] <= a2[i2]:
+            result.append(a1[i1])
+            i1 += 1
         else:
-            new_a.append(a_2[i_2])
-            i_2 += 1
+            result.append(a2[i2])
+            i2 += 1
 
-    while i_1 < len(a_1):
-        new_a.append(a_1[i_1])
-        i_1 += 1
+    result.extend(a1[i1:])
+    result.extend(a2[i2:])
 
-    while i_2 < len(a_2):
-        new_a.append(a_2[i_2])
-        i_2 += 1
-
-    return new_a
+    return result
     
 print(merge_sorted_arrays([1, 4, 7, 20], [3, 5, 6]))
