f849cf76ca2c0e22b7441d40f28154a740ea5302
IvanVazquezL <ivanvl_11@hotmail.com>
2026-03-03 02:07:18 +0000

    correccion sorted_squares

--- TwoPointers/sorted_squares.py
+++ TwoPointers/sorted_squares.py
@@ -1,9 +1,9 @@
 def sorted_squares(nums: list[int]) -> list[int]:
-    n = lens(nums)
+    n = len(nums)
     left, right = 0, n - 1
     result = [0] * n
 
-    for i in range(n, 0, -1):
+    for i in range(n - 1, 0, -1):
         if abs(nums[left]) > abs(nums[right]):
             result[i] = nums[left] ** 2
             left += 1
